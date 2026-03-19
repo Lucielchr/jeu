671d14f085043323de6140a9e815e78f51363648
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2026-03-19 21:14:28 +0100

    + commentaires

--- src/js/hub.js
+++ src/js/hub.js
@@ -53,7 +53,7 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
             key: 'down',
             frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
             frameRate: 10,
-            repeat: -1 
+            repeat: -1
         });
         this.anims.create({
             key: 'left',
@@ -80,9 +80,9 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
         });
 
         // Limites du monde et caméra
-        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels); 
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-        this.player.setCollideWorldBounds(true); 
+        this.player.setCollideWorldBounds(true);
 
         // --- COLLISIONS ---
         if (arbre) {
@@ -107,22 +107,22 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
         }
 
         // Caméra suit le joueur
-        this.cameras.main.startFollow(this.player); 
-        this.cursors = this.input.keyboard.createCursorKeys(); 
+        this.cameras.main.startFollow(this.player);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
         // --- DÉFINITION DES TOUCHES ---
-        this.keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E); 
-        this.keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F); 
+        this.keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+        this.keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
 
         // --- INITIALISATION REGISTRE ---
-        if (this.registry.get('hasCisaille') === undefined) { 
-            this.registry.set('hasCisaille', false); 
+        if (this.registry.get('hasCisaille') === undefined) {
+            this.registry.set('hasCisaille', false);
         }
         if (this.registry.get('hasEpee') === undefined) {
-            this.registry.set('hasEpee', false); 
+            this.registry.set('hasEpee', false);
         }
         if (this.registry.get('dragonVivant') === undefined) {
-            this.registry.set('dragonVivant', true); 
+            this.registry.set('dragonVivant', true);
         }
 
         // --- NOUVEAU : On vérifie si l'intro a déjà été vue ---
@@ -144,12 +144,12 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
             fontSize: '20px',
             fill: '#ffffff',
             backgroundColor: '#000000'
-        }).setOrigin(0.5).setScrollFactor(0); 
-        this.texteAide.setVisible(false); 
+        }).setOrigin(0.5).setScrollFactor(0);
+        this.texteAide.setVisible(false);
 
         // --- LE DRAGON ---
-        this.dragon = this.physics.add.staticSprite(64, 128, 'img6'); 
-        this.physics.add.collider(this.player, this.dragon); 
+        this.dragon = this.physics.add.staticSprite(64, 128, 'img6');
+        this.physics.add.collider(this.player, this.dragon);
 
         if (this.registry.get('dragonVivant') === false) {
             this.dragon.destroy();
@@ -173,13 +173,13 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
             this.texteAide.setText("Appuie sur E pour entrer au Niveau 1");
             this.texteAide.setVisible(true);
             if (Phaser.Input.Keyboard.JustDown(this.keyE)) {
-                this.scene.start('Champignon1'); 
+                this.scene.start('Champignon1');
             }
         }
 
         // --- VÉRIFICATION PORTE 2 ---
         else if (this.physics.overlap(this.player, this.porte2)) {
-            const possedeCisaille = this.registry.get('hasCisaille'); 
+            const possedeCisaille = this.registry.get('hasCisaille');
             if (possedeCisaille) {
                 this.texteAide.setText("Appuie sur E pour entrer au Niveau 2");
                 this.texteAide.setVisible(true);
@@ -201,9 +201,9 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
                 if (possedeEpee) {
                     this.texteAide.setText("Appuie sur F pour terrasser le dragon avec l'épée !");
                     this.texteAide.setVisible(true);
-                    if (Phaser.Input.Keyboard.JustDown(this.keyF)) { 
-                        this.registry.set('dragonVivant', false); 
-                        this.dragon.destroy(); 
+                    if (Phaser.Input.Keyboard.JustDown(this.keyF)) {
+                        this.registry.set('dragonVivant', false);
+                        this.dragon.destroy();
                     }
                 } else {
                     this.texteAide.setText("OHHHH un dragon, il me faut une épée pour le combattre !");
@@ -219,7 +219,7 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
         }
 
         // Mouvements du joueur
-        this.player.setVelocity(0); 
+        this.player.setVelocity(0);
         const speed = 160;
 
         if (this.cursors.left.isDown) {
@@ -232,7 +232,7 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
 
         if (this.cursors.up.isDown) {
             this.player.setVelocityY(-speed);
-            if (!this.cursors.left.isDown && !this.cursors.right.isDown) { 
+            if (!this.cursors.left.isDown && !this.cursors.right.isDown) {
                 this.player.anims.play('up', true);
             }
         } else if (this.cursors.down.isDown) {
@@ -247,45 +247,55 @@ export class Hub extends Phaser.Scene { // Déclare la classe "Hub" qui hérite 
         }
 
         if (this.player.body.velocity.x !== 0 || this.player.body.velocity.y !== 0) {
-            this.player.body.velocity.normalize().scale(speed); 
+            this.player.body.velocity.normalize().scale(speed);
         }
     }
 
     // --- FONCTION POUR LA BULLE ---
     afficherBulleIntroduction(message) {
+        // Crée un conteneur (un groupe vide) placé juste au-dessus du joueur (-60 pixels en Y)
+        // Cela permet de déplacer le texte et le fond en même temps
         let bulleConteneur = this.add.container(this.player.x, this.player.y - 60);
 
+        // Crée l'objet texte avec le message choisi
         let texte = this.add.text(0, 0, message, {
-            fontSize: '14px',
-            fill: '#000000',
-            wordWrap: { width: 180 },
-            align: 'center'
-        }).setOrigin(0.5);
+            fontSize: '14px',        // Taille de la police
+            fill: '#000000',         // Couleur du texte (noir)
+            wordWrap: { width: 180 }, // Le texte revient à la ligne automatiquement après 180 pixels
+            align: 'center'          // Texte centré dans sa zone
+        }).setOrigin(0.5);           // Centre le point de pivot du texte pour qu'il soit bien au milieu
 
+        // Crée un objet graphique pour dessiner le rectangle blanc derrière le texte
         let bg = this.add.graphics();
-        bg.fillStyle(0xffffff, 1);
-        bg.lineStyle(2, 0x000000, 1);
-        bg.fillRoundedRect(-(texte.width/2 + 10), -(texte.height/2 + 10), texte.width + 20, texte.height + 20, 10);
-        bg.strokeRoundedRect(-(texte.width/2 + 10), -(texte.height/2 + 10), texte.width + 20, texte.height + 20, 10);
+        bg.fillStyle(0xffffff, 1);   // Couleur de remplissage (blanc)
+        bg.lineStyle(2, 0x000000, 1); // Bordure de 2 pixels (noire)
 
+        // Dessine un rectangle arrondi calculé selon la taille du texte (+10 pixels de marge)
+        bg.fillRoundedRect(-(texte.width / 2 + 10), -(texte.height / 2 + 10), texte.width + 20, texte.height + 20, 10);
+        bg.strokeRoundedRect(-(texte.width / 2 + 10), -(texte.height / 2 + 10), texte.width + 20, texte.height + 20, 10);
+
+        // Ajoute le fond et le texte dans le conteneur
         bulleConteneur.add([bg, texte]);
+        // Met la bulle au premier plan (profondeur 100) pour qu'elle passe au-dessus des décors
         bulleConteneur.setDepth(100);
 
+        // Crée une boucle qui se répète toutes les 10ms pour que la bulle "colle" au joueur s'il bouge
         const suivreJoueur = this.time.addEvent({
             delay: 10,
             callback: () => {
-                if(bulleConteneur) {
-                    bulleConteneur.x = this.player.x;
-                    bulleConteneur.y = this.player.y - 60;
+                if (bulleConteneur) { // Vérifie que la bulle existe encore
+                    bulleConteneur.x = this.player.x;      // Aligne X sur le joueur
+                    bulleConteneur.y = this.player.y - 60; // Garde la bulle 60px au-dessus
                 }
             },
             loop: true
         });
 
-        // Modifié à 10000ms (10 secondes)
+        // Programme la destruction de la bulle après 10 secondes (10000ms)
         this.time.delayedCall(10000, () => {
-            suivreJoueur.remove();
-            if(bulleConteneur) bulleConteneur.destroy();
+            suivreJoueur.remove(); // Arrête la boucle de suivi
+            if (bulleConteneur) bulleConteneur.destroy(); // Supprime visuellement la bulle
         });
     }
+
 }
